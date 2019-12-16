e2825202d7e8001ad75dba0250f169dd1e9b64cf
rubenskleinschmidtjr@gmail.com <pacoloco09>
2019-12-16 14:39:15 -0300

    Initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hbparking'`, () => {
+  it(`should have as title 'Hb Parking'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hbparking');
+    expect(app.title).toEqual('Hb Parking');
   });
 
   it('should render title', () => {
