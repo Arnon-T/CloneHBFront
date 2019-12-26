e4c75a41eb1cd20d4ac0b5eee3f223639b62908c
gustavo.martins <gustavofmartins44@gmail.com>
2019-12-26 08:53:00 -0300

    Criaçao tela colaboradores e integracao api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { VehicleModelComponent } from './import-model/import-model.component';
 import { RentalTermService } from './rental-term/rental-term.service';
 import { MarcaService } from './marca/marca.service';
 import { ModelService } from './import-model/import-model.service';
+import { CadastroColaboradoresComponent } from './cadastro-colaboradores/cadastro-colaboradores.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { ModelService } from './import-model/import-model.service';
     HeaderComponent,
     SelectpageimportComponent,
     MarcaComponent,
-    VehicleModelComponent
+    VehicleModelComponent,
+    CadastroColaboradoresComponent
   ],
   imports: [
     BrowserModule,
