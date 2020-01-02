43905f0c3d1082279a1040e8c82e05315f931bef
rubenskleinschmidtjr@gmail.com <pacoloco09>
2020-01-02 15:41:16 -0300

    Estilizando paginas de configuracoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainPageComponent } from './main-page/main-page.component';
 import { RentalTermComponent } from './rental-term/rental-term.component';
-import { SelectpageimportComponent } from './selectpageimport/selectpageimport.component';
+import { FuncionalityPageComponent } from './funcionality-page/funcionality-page.component';
 import { MarcaComponent } from './marca/marca.component';
 import { VehicleModelComponent } from './import-model/import-model.component';
 import { CadastroColaboradoresComponent } from './cadastro-colaboradores/cadastro-colaboradores.component';
@@ -14,7 +14,7 @@ import { NotfoundComponent } from './notfound/notfound.component';
 const routes: Routes = [
   { path: "", component: MainPageComponent },
   { path: "termo-locacao", component: RentalTermComponent },
-  { path: "gerenciar", component: SelectpageimportComponent },
+  { path: "gerenciar", component: FuncionalityPageComponent },
   { path: "gerenciar/marcas", component: MarcaComponent },
   { path: "gerenciar/modelos", component: VehicleModelComponent },
   { path: "gerenciar/colaboradores", component: CadastroColaboradoresComponent },
